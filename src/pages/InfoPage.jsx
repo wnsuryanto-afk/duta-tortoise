725072397da06173bc0da6d271bebd22c6da92b0
Iwan Suryanto <wnsuryanto@gmail.com>
2026-05-21 16:53:57 +0000

    File changes

--- src/pages/InfoPage.jsx
+++ src/pages/InfoPage.jsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { useState, useRef } from "react";
 import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { base44 } from "@/api/base44Client";
